26fabbfe26569006002d019b782eefe18d258792
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-13 14:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const divide = (arr, n) => {
-	let subarr = [] , ans = [], tracker = 0;
-	if(arr.length === 0) return [];
-	for(let i = 0 ; i < arr.length; i++){
-		if(tracker + arr[i] <= n){
-			subarr.push(arr[i]);
-			tracker += arr[i];
-		}else{
-			ans.push(subarr);
-			subarr = [arr[i]];
-			tracker = arr[i];
-		}
-	}
+    let subarr = [],
+        ans = [],
+        tracker = 0;
 
-	ans.push(subarr);
-	return ans;
+    if (arr.length === 0) return [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (tracker + arr[i] <= n) {
+            subarr.push(arr[i]);
+            tracker += arr[i];
+        } else {
+            ans.push(subarr);
+            subarr = [arr[i]];
+            tracker = arr[i];
+        }
+    }
+
+    ans.push(subarr);
+    return ans;
 };
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
