ffc3ecd19dd58f6f635003f372e18fda4ac82a04
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-13 14:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
 	let subarr = [], ans = [], tracker = 0;
-	for(let i =0 ; i< arr.length; i++){
+	for(let i = 0 ; i< arr.length; i++){
 		if(tracker + arr[i] <= n){
 			subarr.push(arr[i]);
-			tracking += arr[i];
+			tracker += arr[i];
 		}else{
 			ans.push(subarr);
 			subarr = [arr[i]];
